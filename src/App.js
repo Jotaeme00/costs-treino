6f54561377642fb65b310e1560649a1da8f09673
Juan Muniz <juan@leao.com>
2025-03-07 17:04:04 -0300

    Navbar e Footer

--- src/App.js
+++ src/App.js
@@ -3,28 +3,26 @@ import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <div>  
-        <Link to= "/">Home</Link>
-        <Link to= "/company"> Company</Link>
-        <Link to= "/contato">Contato</Link>
-        <Link to= "/newproject">New Project</Link>
-      </div>
-
+      <Navbar />
       <Container customClass="min-height"> 
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/company" element={<Company />} />
           <Route path="/contato" element={<Contact />} />
           <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
